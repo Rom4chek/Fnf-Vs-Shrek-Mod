1d28aae594ef81a256b01f41a80452a0c8b12607
Rom4chek <Animation370@gmail.com>
2022-03-06 14:38:18 +0600

    beep

--- source/Main.hx
+++ source/Main.hx
@@ -76,7 +76,7 @@ class Main extends Sprite
 		}
 
 		#if cpp
-		initialState = Caching; 
+		initialState = Caching; //TitleState;
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
 		#else
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
@@ -94,23 +94,6 @@ class Main extends Sprite
 	var game:FlxGame;
 
 	var fpsCounter:FPS;
-
-	public static function dumpCache() // HAHA I STEAL THE CODE HAHAHAHAH
-		{
-			@:privateAccess
-			for (key in FlxG.bitmap._cache.keys())
-			{
-				var obj = FlxG.bitmap._cache.get(key);
-				if (obj != null)
-				{
-					Assets.cache.removeBitmapData(key);
-					FlxG.bitmap._cache.remove(key);
-					obj.destroy();
-				}
-			}
-			Assets.cache.clear("songs");
-			// */
-		}
 
 	public function toggleFPS(fpsEnabled:Bool):Void {
 		fpsCounter.visible = fpsEnabled;
